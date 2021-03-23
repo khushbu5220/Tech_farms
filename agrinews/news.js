4e3e75297e42bd696a9b7138a2bcea6b8ade1945
Khushbu <singhkhushbu210@gmail.com>
2021-03-24 02:45:15 +0530

    add readme

--- agrinews/news.js
+++ agrinews/news.js
@@ -1,10 +1,39 @@
-fetch('http://newsapi.org/v2/everything?q=Agriculture&from=2021-02-27&sortBy=popularity?country=in&apiKey=7492ea061b604d5b87f1759168417673')
-.then(function (response) {
-return response.json();
-}) 
-.then(function (news) {
-    console.log(news);
-}).catch(function (error) {
-    console.error('went wrong');
-    console.error('error');
-})
+// // fetch("http://newsapi.org/v2/everything?q=Agriculture&from=2021-02-27&sortBy=popularity?country=in&apiKey=7492ea061b604d5b87f1759168417673")
+// // .then(response => response.json())
+// // .then(data => {
+// //     console.log(data.status)
+// //     document.querySelector("name").innerText = data.status
+// // })
+
+
+
+// // Example POST method implementation:
+// async function postData(url = '', data = {}) {
+//     // Default options are marked with *
+//     const response = await fetch(url, {
+//       method: 'POST', // *GET, POST, PUT, DELETE, etc.
+//       mode: 'cors', // no-cors, *cors, same-origin
+//       cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+//       credentials: 'same-origin', // include, *same-origin, omit
+//       headers: {
+//         'Content-Type': 'application/json'
+//         // 'Content-Type': 'application/x-www-form-urlencoded',
+//       },
+//       redirect: 'follow', // manual, *follow, error
+//       referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+//       body: JSON.stringify(data) // body data type must match "Content-Type" header
+//     });
+//     return response.json(); // parses JSON response into native JavaScript objects
+//   }
+  
+//   postData('http://newsapi.org/v2/everything?q=Agriculture&from=2021-02-27&sortBy=popularity?country=in&apiKey=7492ea061b604d5b87f1759168417673', { answer: 42 })
+//     .then(data => {
+//       console.log(data); // JSON data parsed by `data.json()` call
+//     });
+
+
+
+
+
+
+
